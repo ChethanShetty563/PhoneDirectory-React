98b12842738fca30c1cd9d6e765741be5386489d
ChethanShetty563 <chethan563@gmail.com>
2021-01-20 12:38:52 +0530

    External styling

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
+import './Header.css';
 
 const Header = function() {
-    const headerStyle = {textAlign : 'center',padding:20,backgroundColor: '#000',color: '#fff', textTransform:'uppercase'}
     return (
-        <div style={headerStyle}>
+        <div className="header">
         Phone Directory
     </div>
     );
